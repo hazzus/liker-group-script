609e7b71992504dd3f23c207bfb2a9ebcc6e035f
Pavel Bezborodov <dfa.sro591@gmail.com>
2018-02-20 19:26:14 +0300

    Information class defined Token check made separately

---
+++ information.py
@@ -0,0 +1,143 @@
+import time
+import webbrowser as wb
+from urllib.parse import urlparse, parse_qsl
+
+import vk
+from vk.exceptions import VkAPIError
+
+
+class WorkInformation:
+    FILENAME = 'variables.log'
+    TOKEN_FILE = 'token.log'
+    V = '5.73'
+
+    session = None
+    api = None
+    token = None
+    likes_amount = None
+    delay = None
+    group = None
+    got = None
+
+    def __init__(self):
+        while not self.check_token():
+            self.auth()
+        if not self.check_variables():
+            self.get_information_from_user()
+        else:
+            self.get_information_from_file()
+
+    'Authentication'
+
+    def do_session_api(self):
+        self.session = vk.Session(access_token=self.token)
+        self.api = vk.API(self.session)
+
+    def auth(self):
+        print('Токена нет, или он неверный, сейчас будет регистрация')
+        site = 'https://oauth.vk.com/authorize?client_id=6004708&' \
+               'redirect_uri=https://oauth.vk.com/blank.html&scope=wall&response_type=token&v=5.73'
+        print('ВНИМАНИЕ. Через 6 секунд откроется сайт аутентификации, не закрывайте вкладку после разрешения доступа')
+        time.sleep(6)
+        wb.open(site)
+        try:
+            parse_dict = dict(parse_qsl(urlparse(input('Введите ссылку/поле адреса открывшегося сайта: ')).fragment))
+            self.token = parse_dict[u'access_token']
+            live_time = parse_dict[u'expires_in']
+            got_time = str(time.time())
+            self.write_token(live_time, got_time)
+        except KeyError:
+            print('Неверный токен(не найден). Запустите заново и разрешите доступ аутентификации')
+            quit()
+
+    'Checkers'
+
+    def check_token(self):
+        num_lines = sum(1 for line in open(self.TOKEN_FILE))
+        if num_lines != 3:
+            return False
+        with open(self.TOKEN_FILE, 'r') as t:
+            token = t.readline()
+            self.token = token[:len(token) - 1]
+            live_time = t.readline()
+            live_time = int(live_time[:len(live_time) - 1])
+            got_time = t.readline()
+            got_time = float(got_time[: len(got_time) - 1])
+            if time.time() - got_time > live_time:
+                print('Токен просрочен!')
+                return False
+        self.do_session_api()
+        try:
+            self.api.users.get(user_ids=1, v=self.V)
+        except VkAPIError as e:
+            if e.code == 5:
+                print('Токен неверный!')
+            else:
+                print(e)
+            return False
+        return True
+
+    def check_variables(self):
+        continuing = True
+        num_lines = sum(1 for line in open(self.FILENAME))
+        with open(self.FILENAME, 'r') as log:
+            if (len(log.read()) == 0) or (num_lines != 4):
+                continuing = False
+        return continuing
+
+    'Gathering information'
+
+    @staticmethod
+    def get_group_name():
+        while True:
+            group = urlparse(input('Ссылка на группу/короткий домен/id: ')).path
+            if group[0] == '/':
+                group = group[1:]
+            print('Домен/id группы - ', group, 'верно?(y/n)')
+            ok = input()
+            if ok == 'y':
+                break
+            else:
+                print('Повторите ввод группы по-другому')
+        return group
+
+    def get_information_from_user(self):
+        print('Начинаем новый процесс!')
+        self.likes_amount = input('Кол-во лайков("inf" - до конца стены): ')
+        self.delay = float(input('Задержка после запросов(милисекунды, влияет на частоту капчи и скорость прохода,'
+                                 ' рекомендую ~900-1500): ')) / 1000
+        self.group = self.get_group_name()
+        self.got = 0
+
+    def get_information_from_file(self):
+        print('Запускаю продолжение процесса')
+        log = open(self.FILENAME, 'r')
+        likes_amount = log.readline()
+        self.likes_amount = likes_amount[:len(likes_amount) - 1]
+        delay = log.readline()
+        self.delay = float(delay[:len(delay) - 1])
+        group = log.readline()
+        self.group = group[:len(group) - 1]
+        self.got = int(log.readline())
+        log.close()
+
+    'Writers'
+
+    def write_token(self, lt, gt):
+        with open(self.TOKEN_FILE, 'w') as t:
+            t.write(self.token + '\n')
+            t.write(lt + '\n')
+            t.write(gt + '\n')
+
+    def log_write(self):
+        log = open(self.FILENAME, 'w')
+        log.write(self.likes_amount + '\n')
+        log.write(str(self.delay) + '\n')
+        log.write(self.group + '\n')
+        log.write(str(self.got) + '\n')
+        log.close()
+
+    def clear_log(self):
+        log = open(self.FILENAME, 'w')
+        log.write('')
+        log.close()
--- main.py
+++ main.py
@@ -1,91 +1,13 @@
-import vk
 import time
-from urllib.parse import urlparse, parse_qsl
 from urllib.request import urlopen
 from PIL import Image
-import webbrowser as wb
 
 from vk.exceptions import VkAPIError
+from information import WorkInformation
 
-global token
-global likes_amount
-global delay
-global group
-global got
-filename = 'variables.log'
-V = '5.73'
 COUNT = 1000
 
-continuing = True
-num_lines = sum(1 for line in open(filename))
-with open(filename, 'r') as log:
-    if (len(log.read()) == 0) or (num_lines != 5):
-        continuing = False
-
-
-def get_group():
-    while True:
-        group = urlparse(input('Ссылка на группу/короткий домен/id: ')).path
-        if group[0] == '/':
-            group = group[1:]
-        print('Домен/id группы - ', group, 'верно?(y/n)')
-        ok = input()
-        if ok == 'y':
-            break
-        else:
-            print('Повторите ввод группы по-другому')
-    return group
-
-
-if not continuing:
-    print('Начинаем новый процесс!')
-    site = 'https://oauth.vk.com/authorize?client_id=6004708&' \
-           'redirect_uri=https://oauth.vk.com/blank.html&scope=wall&response_type=token&v=5.73'
-    print('ВНИМАНИЕ. Через 6 секунд откроется сайт аутентификации, не закрывайте вкладку после разрешения доступа')
-    time.sleep(6)
-    wb.open(site)
-    try:
-        token = dict(parse_qsl(urlparse(input('Введите ссылку/поле адреса открывшегося сайта: ')).fragment))[u'access_token']
-    except KeyError:
-        print('Неверный токен(не найден). Запустите заново и разрешите доступ аутентификации')
-        quit()
-    likes_amount = input('Кол-во лайков("inf" - до конца стены): ')
-    delay = float(input('Задержка после запросов(милисекунды, влияет на частоту капчи и скорость прохода,'
-                        ' рекомендую ~900-1500): ')) / 1000
-    group = get_group()
-    got = 0
-else:
-    print('Запускаю продолжение процесса')
-    log = open(filename, 'r')
-    token = log.readline()
-    token = token[:len(token) - 1]
-    likes_amount = log.readline()
-    likes_amount = likes_amount[:len(likes_amount) - 1]
-    delay = log.readline()
-    delay = float(delay[:len(delay) - 1])
-    group = log.readline()
-    group = group[:len(group) - 1]
-    got = int(log.readline())
-    log.close()
-
-session = vk.Session(access_token=token)
-vk_api = vk.API(session)
-
-
-def log_write(number):
-    log = open(filename, 'w')
-    log.write(token + '\n')
-    log.write(likes_amount + '\n')
-    log.write(str(delay) + '\n')
-    log.write(group + '\n')
-    log.write(str(number) + '\n')
-    log.close()
-
-
-def clear_log():
-    log = open(filename, 'w')
-    log.write('')
-    log.close()
+global info
 
 
 def open_image(captcha_url):
@@ -105,18 +27,18 @@ def like(user_id, amount):
     while True:
         posts = None
         try:
-            posts = vk_api.wall.get(owner_id=user_id, offset=off, count=100, filter='owner', v=V)[u'items']
+            posts = info.api.wall.get(owner_id=user_id, offset=off, count=100, filter='owner', v=info.V)[u'items']
         except VkAPIError as error:
             if error.is_captcha_needed():
                 captcha = captcha_cover(error)
-                posts = vk_api.wall.get(owner_id=user_id, offset=off, count=100, filter='owner', v=V,
-                                        captcha_sid=captcha[1], captcha_key=captcha[0])[u'items']
+                posts = info.api.wall.get(owner_id=user_id, offset=off, count=100, filter='owner', v=info.V,
+                                          captcha_sid=captcha[1], captcha_key=captcha[0])[u'items']
             elif error.code == 6:
                 time.sleep(1)
-                posts = vk_api.wall.get(owner_id=user_id, offset=off, count=100, filter='owner', v=V)[u'items']
+                posts = info.api.wall.get(owner_id=user_id, offset=off, count=100, filter='owner', v=info.V)[u'items']
             else:
                 print(error)
-        time.sleep(delay)
+        time.sleep(info.delay)
         if (not posts) or (str(liked) == amount):
             break
         else:
@@ -125,54 +47,59 @@ def like(user_id, amount):
                 if str(liked) == amount:
                     break
                 if count % 3 == 0:
-
+                    print('Лайкаю пост ' + str(p[u'id']) + ' пользователя ' + str(user_id))
                     try:
-                        vk_api.likes.add(type='post', owner_id=user_id, item_id=p[u'id'], v=V)
+                        info.api.likes.add(type='post', owner_id=user_id, item_id=p[u'id'], v=info.V)
                     except VkAPIError as error:
                         if error.is_captcha_needed():
                             captcha = captcha_cover(error)
-                            vk_api.likes.add(type='post', owner_id=user_id, item_id=p[u'id'], v=V,
-                                             captcha_sid=captcha[1], captcha_key=captcha[0])
+                            info.api.likes.add(type='post', owner_id=user_id, item_id=p[u'id'], v=info.V,
+                                               captcha_sid=captcha[1], captcha_key=captcha[0])
                         else:
                             print(error)
-                    time.sleep(delay)
+                    time.sleep(info.delay)
                     liked += 1
                 count += 1
 
 
-log_write(got)
-while True:
-    try:
-        members = None
+def work():
+    while True:
         try:
-            members = vk_api.groups.getMembers(group_id=group, offset=got, count=COUNT, v=V)[u'items']
-        except VkAPIError as error:
-            if error.is_captcha_needed():
-                captcha = captcha_cover(error)
-                members = vk_api.groups.getMembers(group_id=group, offset=got, count=COUNT, v=V, captcha_sid=captcha[1],
-                                                   captcha_key=captcha[1])[u'items']
-            elif error.code == 6:
-                time.sleep(1)
-                members = vk_api.groups.getMembers(group_id=group, offset=got, count=COUNT, v=V)[u'items']
-            elif error.code == 125:
-                print('Неверный домен группы, попробуйте еще раз')
-                print(group)
-                group = get_group()
-            else:
-                print(error)
-                clear_log()
-                print('В логах произошла ошибка, они сброшены, перезапустите процесс')
+            members = None
+            try:
+                members = info.api.groups.getMembers(group_id=info.group, offset=info.got, count=COUNT, v=info.V)[u'items']
+            except VkAPIError as error:
+                if error.is_captcha_needed():
+                    captcha = captcha_cover(error)
+                    members = \
+                        info.api.groups.getMembers(group_id=info.group, offset=info.got, count=COUNT, v=info.V,
+                                                   captcha_sid=captcha[1],
+                                                   captcha_key=captcha[0])[u'items']
+                elif error.code == 6:
+                    time.sleep(1)
+                    members = info.api.groups.getMembers(group_id=info.group, offset=info.got,
+                                                         count=COUNT, v=info.V)[u'items']
+                elif error.code == 125:
+                    print('Неверный домен группы, попробуйте еще раз')
+                    print(info.group)
+                    info.group = info.get_group_name()
+                else:
+                    print(error)
+            time.sleep(info.delay)
+            if not members:
+                print('Дело сделано')
                 break
-        time.sleep(delay)
-        if not members:
-            print('Дело сделано')
-            break
-        else:
-            for u_id in members:
-                like(u_id, likes_amount)
-                got += 1
-                log_write(got)
-    except (KeyboardInterrupt, SystemExit) as e:
-        log_write(got)
-        quit()
-    time.sleep(2)
+            else:
+                for u_id in members:
+                    like(u_id, info.likes_amount)
+                    info.got += 1
+                    info.log_write()
+        except (KeyboardInterrupt, SystemExit) as e:
+            info.log_write()
+            quit()
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    info = WorkInformation()
+    work()

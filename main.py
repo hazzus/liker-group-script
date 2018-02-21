5c834d7dc37f5741447f231bf48e7dc8cd62747a
Pavel Bezborodov <dfa.sro591@gmail.com>
2018-02-21 13:07:06 +0300

    Exception on wrong group name now handled

--- main.py
+++ main.py
@@ -42,7 +42,9 @@ def like(user_id, amount):
                         if e.is_captcha_needed():
                             time.sleep(info.delay)
                         else:
-                            break
+                            print(e)
+                            print('Непредвиденная ошибка')
+                            quit()
             elif error.code == 6:
                 print('Слишком много запросов, подождем 1 секунду...')
                 time.sleep(1)
@@ -75,7 +77,9 @@ def like(user_id, amount):
                                     if e.is_captcha_needed():
                                         time.sleep(info.delay)
                                     else:
-                                        break
+                                        print(e)
+                                        print('Непредвиденная ошибка')
+                                        quit()
                         elif error.code == 6:
                             print('Слишком много запросов, подождем 1 секунду..')
                             time.sleep(1)
@@ -89,52 +93,49 @@ def like(user_id, amount):
 
 def work():
     while True:
+        members = None
         try:
-            members = None
-            try:
+            members = info.api.groups.getMembers(group_id=info.group, offset=info.got,
+                                                 count=COUNT, v=info.V)[u'items']
+        except VkAPIError as error:
+            if error.is_captcha_needed():
+                while True:
+                    try:
+                        captcha = captcha_cover(error)
+                        members = \
+                            info.api.groups.getMembers(group_id=info.group, offset=info.got, count=COUNT, v=info.V,
+                                                       captcha_sid=captcha[1],
+                                                       captcha_key=captcha[0])[u'items']
+                        break
+                    except VkAPIError as e:
+                        if e.is_captcha_needed():
+                            time.sleep(info.delay)
+                        else:
+                            print(e)
+                            print('Непредвиденная ошибка')
+                            quit()
+            elif error.code == 6:
+                print('Слишком много запросов, подождем 1 секунду..')
+                time.sleep(1)
                 members = info.api.groups.getMembers(group_id=info.group, offset=info.got,
                                                      count=COUNT, v=info.V)[u'items']
-            except VkAPIError as error:
-                if error.is_captcha_needed():
-                    while True:
-                        try:
-                            captcha = captcha_cover(error)
-                            members = \
-                                info.api.groups.getMembers(group_id=info.group, offset=info.got, count=COUNT, v=info.V,
-                                                           captcha_sid=captcha[1],
-                                                           captcha_key=captcha[0])[u'items']
-                            break
-                        except VkAPIError as e:
-                            if e.is_captcha_needed():
-                                time.sleep(info.delay)
-                            else:
-                                break
-                elif error.code == 6:
-                    print('Слишком много запросов, подождем 1 секунду..')
-                    time.sleep(1)
-                    members = info.api.groups.getMembers(group_id=info.group, offset=info.got,
-                                                         count=COUNT, v=info.V)[u'items']
-                elif error.code == 125:
-                    print('Неверный домен группы - ' + info.group + ', попробуйте еще раз')
-                    info.group = info.get_group_name()
-                    info.write_vars()
-                    members = info.api.groups.getMembers(group_id=info.group, offset=info.got,
-                                                         count=COUNT, v=info.V)[u'items']
-                else:
-                    print(error)
-            time.sleep(info.delay)
-            if not members:
-                print('Дело сделано')
-                break
+            elif error.code == 125:
+                print('Неверный домен группы - ' + info.group + ', попробуйте еще раз')
+                info.group = info.get_group_name()
+                info.write_vars()
+                members = info.api.groups.getMembers(group_id=info.group, offset=info.got,
+                                                     count=COUNT, v=info.V)[u'items']
             else:
-                for u_id in members:
-                    like(u_id, info.likes_amount)
-                    info.got += 1
-                    info.write_vars()
-        except (KeyboardInterrupt, SystemExit):
-            info.write_vars()
-            print('Процесс прерван пользователем, промежуточный результат сохранен')
-            quit()
+                print(error)
+        time.sleep(info.delay)
+        if not members:
+            print('Дело сделано')
+            break
+        else:
+            for u_id in members:
+                like(u_id, info.likes_amount)
+                info.got += 1
+                info.write_vars()
         time.sleep(2)
 
 
@@ -148,3 +149,7 @@ if __name__ == '__main__':
             info.write_vars()
     except ConnectionError:
         print('Интернет соединение не установлено')
+    except (KeyboardInterrupt, SystemExit):
+        if info is not None:
+            info.write_vars()
+        print('Процесс прерван пользователем, промежуточный результат сохранен')
